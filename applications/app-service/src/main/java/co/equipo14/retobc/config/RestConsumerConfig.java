3e802182e7e71f23d903f6e1f67bcd4057357b75
fortemon <blackforte@gmail.com>
2021-02-15 15:44:26 -0500

    Crea estructura driven adapter

--- applications/app-service/src/main/java/co/equipo14/retobc/config/RestConsumerConfig.java
+++ applications/app-service/src/main/java/co/equipo14/retobc/config/RestConsumerConfig.java
@@ -1,14 +1,23 @@
 package co.equipo14.retobc.config;
 
-import co.equipo14.retobc.usecase.saldosymovimientos.SaldosYMovimientosUseCase;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class RestConsumerConfig {
 
+    @Value("${adapter.restconsumer.url}")
+    private String url;
+
     @Bean
-    public SaldosYMovimientosUseCase getSaldosYMovimientosUseCase(){
-        return new SaldosYMovimientosUseCase();
+    public WebClient getWebClient() {
+        return WebClient.builder()
+            .baseUrl(url)
+            .defaultHeader(HttpHeaders.CONTENT_TYPE, "application/json")
+            .build();
     }
+
 }
